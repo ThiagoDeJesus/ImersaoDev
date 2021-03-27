5a424063b21bc1fe44b434f6f4d1e4994ba70682
Thiago de Jesus <99thiagodejesus@gmail.com>
2021-03-27 12:14:32 -0300

    Adicionado Desafio 2, e algumas modificações

---
+++ desafio2/script.js
@@ -0,0 +1,8 @@
+function chutar() {
+    var chute = prompt("Digite o número da opção.\n1-Pikachu de Chernobyl\n2-Charmander Bugado\n3-Ratata que deu errado")
+    if (chute == 1) {
+        alert("Parabêns, você acertou!")
+    } else {
+        alert("Eita, errou, mais sorte na próxima ;)")
+    }
+}
